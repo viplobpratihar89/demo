82099a62f8db5f4242f9559bcc00660bc08ff68e
viplob <viplobp@gmail.com>
2017-06-10 12:43:06 +0530

    first commit

---
+++ app/src/main/java/com/motrss/www/gitdemo/EmptyActivity.java
@@ -0,0 +1,13 @@
+package com.motrss.www.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
